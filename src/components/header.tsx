d3238858578e727c9f8bd6f5d468f7bdda4ba793
Sandeep Prasad <s3g007@gmail.com>
2022-09-27 23:19:00 -0400

    Fixed title position issue.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 export function Header(props: { children: any }) {
   return (
     <div className='flex justify-center mt-4'>
-      <h1 className='fixed font-sans text-6xl font-extralight uppercase -rotate-90 bottom-[30vw] -left-20 text-neutral-800'>
+      <h1 className='fixed font-sans text-6xl font-extralight uppercase -rotate-90 bottom-[50%] -left-20 text-neutral-800'>
         Music Pad
       </h1>
       {props.children}
