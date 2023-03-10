b498ebcbdb9e40e2c42f93a85b9aad5736be8ce6
Sandeep Prasad <s3g007@gmail.com>
2023-03-09 23:44:35 -0500

    Adjusted title for notch.

--- src/components/AppTitle.tsx
+++ src/components/AppTitle.tsx
@@ -2,7 +2,7 @@ import { HTMLAttributes } from 'react';
 
 export function AppTitle({ children }: HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className='flex justify-center mt-4'>
+    <div className='flex justify-center mt-12'>
       <h1 className='font-sans text-6xl font-bold text-transparent uppercase bg-clip-text bg-gradient-to-r from-indigo-500 to-pink-900'>
         Music Pad
       </h1>
